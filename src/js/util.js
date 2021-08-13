435f08979ec2b0a042158017c58bdefc5a88bcbb
nandopedrosa <fpedrosa@gmail.com>
2021-08-13 11:00:55 -0300

    Requisitos

--- src/js/util.js
+++ src/js/util.js
@@ -1,5 +1,9 @@
 //Busca um item aleatorio em um array. Retorna o item e seu índice
 function getRandomItem(arrayDeItens) {
+    if(arrayDeItens.length == 0) {
+        return null;
+    }
+    
     //Numero aleatorio entre 0 e o último item do array
     const index = Math.floor(Math.random() * ((arrayDeItens.length - 1) + 1)) + 0
     const item = arrayDeItens[index];
@@ -7,12 +11,33 @@ function getRandomItem(arrayDeItens) {
 }
 
 //Carrega um game aleatorio
+//Basicamente carregamos todos os links de Jogar da Home e escolhemos algum aleatório
+//Carrega um game aleatorio
+//Basicamente carregamos todos os links de Jogar da Home e escolhemos algum aleatório
 function estouComSorte() {
-    const games = [];
-    games.push('gov-itil4-praticas');
 
-    const game = getRandomItem(games).objeto;
+    const xhttp = new XMLHttpRequest();
 
-    window.location.href = "/src/games/" + game + ".html";
+    // envio da requisicao
+    xhttp.open("GET", "/src/home.html");
+    xhttp.send();
+
+
+    //callback
+    xhttp.onload = function () {
+        const homeHtml = this.responseText;
+        var games = [];
+
+        $(homeHtml).find('a.jogar').each(function () {
+            const url = $(this).attr('href');
+            games.push(url);
+        });
+
+        const pagina = getRandomItem(games);
+        var currentUrl = window.location;
+        var baseUrl = currentUrl.protocol + "//" + currentUrl.host + "//";
+        window.location.href = baseUrl + pagina.objeto;
+    }
+
 }
 
